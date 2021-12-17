a6e6d075e30b08fc124681d3887a84b820df68fb
josealex7 <alevx197715@gmail.com>
2021-12-17 01:46:05 -0500

    Sweet Alert Agregado

--- src/components/LogOut.js
+++ src/components/LogOut.js
@@ -1,10 +1,11 @@
 import { Refresh } from "@material-ui/icons"
 import React from "react"
 
+
 export const LogOut = () =>{
     let objectUSer = {
         exist:false
     }
     localStorage.setItem('Auth', JSON.stringify(objectUSer))
-    window.location.href=window.location.href+"/*"
+    window.location.reload(true);
 }
